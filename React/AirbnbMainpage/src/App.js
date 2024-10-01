c42cc588a561b5df54e8d1485bb258c5c6f51bcd
mfyx <1571547820@qq.com>
2024-10-01 10:15:47 +0800

    基于props实现组件数据的加载

--- React/AirbnbMainpage/src/App.js
+++ React/AirbnbMainpage/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 import MainContent from "./MainContent";
 import Hero from "./Hero";
 import Card from "./Card";
+import img1 from "./hero_1.jpg";
 
 
 export default function App() {
@@ -12,7 +13,14 @@ export default function App() {
     <div className="Container">
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+        img={img1}
+        rating={5.0}
+        reviews={6}
+        country="USA"
+        price="136"
+        title="Life Lessons with Katie Zaferes"
+      />
       {/* <h1>App component</h1> */}
       {/* <Navbar /> */}
       {/* <MainContent /> */}
--- React/AirbnbMainpage/src/Card.js
+++ React/AirbnbMainpage/src/Card.js
@@ -1,19 +1,31 @@
 import React from "react";
-import image from "./hero_1.jpg";
 import star from "./star.jpg";
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props);
     return (
         <div className="card">
-            <img src={image} className="card--image" />
+            <img 
+                src={props.img} 
+                className="card--image" 
+                alt="Main card image."
+            />
             <div className="card--stats">
-                <img src={star} className="card--star"/>
-                <span>5.0</span>
-                <span className="grey">(6) · </span>
-                <span className="grey">USA</span>
+                <img 
+                    src={star} 
+                    className="card--star"
+                    alt="Star icom."
+                />
+                <span>{props.rating}</span>
+                {/* <span>5.0</span> */}
+                <span className="grey">({props.reviewCount}) · </span>
+                <span className="grey">{props.country}</span>
+                {/* <span className="grey">USA</span> */}
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / preson</p>
+            <h2>{props.title}</h2>
+            {/* <h2>Life Lessons with Katie Zaferes</h2> */}
+            <p><span className="bold">From ${props.price}</span> / preson</p>
+            {/* <p><span className="bold">From $136</span> / preson</p> */}
         </div>
     )
 }
