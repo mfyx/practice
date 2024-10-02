6acc1aa28b1c46172065c18aa7eb9fd563dbd680
mfyx <1571547820@qq.com>
2024-10-02 22:02:12 +0800

    完成MemeGenerator练习

--- React/MemeGenerator/src/Meme.js
+++ React/MemeGenerator/src/Meme.js
@@ -1,5 +1,5 @@
 import React, { Component } from'react';
-import memesData from './memesData';
+//import memesData from './memesData';
 
 export default function Meme(props) {
     //const [memeImage, setMemeImage] = React.useState('http://zhenyutongxue.com/usr/uploads/2024/09/2712895295.jpg');
@@ -9,14 +9,32 @@ export default function Meme(props) {
         randomImage: 'http://zhenyutongxue.com/usr/uploads/2024/09/2712895295.jpg'
     });
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+    const [allMemes, setAllMeme] = React.useState([]);
+
+    React.useEffect(() => {
+        // fetch('https://api.imgflip.com/get_memes')
+        //    .then(response => response.json())
+        //    .then(data => setAllMeme(data.data.memes))
+
+        // the version of use async function
+        async function getMemes() {
+            const res = await fetch('https://api.imgflip.com/get_memes');
+            const data = await res.json();
+            setAllMeme(data.data.memes);
+        }
+
+        getMemes();
+
+        return () => {
+            // cleanup
+        };
+    }, []);
 
     function getMemeImage(event) {
         event.preventDefault();
 
-        const memesArray = allMemeImages.data.memes;
-        const randomIndex = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randomIndex].url;
+        const randomIndex = Math.floor(Math.random() * allMemes.length);
+        const url = allMemes[randomIndex].url;
 
         setMeme(prevMeme => ({
             ...prevMeme, 
@@ -24,15 +42,28 @@ export default function Meme(props) {
         }));
     }
 
+    function handleChange(event) {
+        event.preventDefault();
+        const {name, value} = event.target;
+
+        setMeme(prevMeme => ({
+            ...prevMeme, 
+             [name]: value 
+        }));
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 {/* <label htmlFor='top-text'>Top Text</label> */}
                 <input 
                     id="top-text" 
                     type="text" 
                     className="form--input" 
                     placeholder="Top text" 
+                    name="topText" 
+                    value={meme.topText} 
+                    onChange={handleChange}
                 />
                 
                 {/* <label htmlFor='bottom-text'>Bottom Text</label> */}
@@ -41,6 +72,9 @@ export default function Meme(props) {
                     type="text" 
                     className='form--input' 
                     placeholder='Bottom text'
+                    name="bottomText" 
+                    value={meme.bottomText} 
+                    onChange={handleChange}
                 />
 
                 <button 
@@ -50,9 +84,13 @@ export default function Meme(props) {
                     Get a new meme image 🖼
                 </button>
 
-            </form>
-            {/* <p>{memeImage}</p> */}
-            <img className="meme--image" src={meme.randomImage} alt="Meme" />
+            </div>
+
+            <div className="meme">
+                <img className="meme--image" src={meme.randomImage} alt="Meme" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     );
 }
