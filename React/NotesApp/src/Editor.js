d0050205644deccbc853d20a392d90e7483d9f97
mfyx <1571547820@qq.com>
2024-10-03 13:23:38 +0800

    初步构建NotesApp

---
+++ React/NotesApp/src/Editor.js
@@ -0,0 +1,50 @@
+import React from "react"
+import ReactMde from "react-mde"
+import Showdown from "showdown"
+
+export default function Editor({ currentNote, updateNote }) {
+    const [selectedTab, setSelectedTab] = React.useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })
+
+    // 定义只显示的工具栏命令
+    const toolbarCommands = [
+        [
+            {
+                name: "preview",
+                //icon: "eye",
+                execute: () => setSelectedTab("preview"),
+            },
+            {
+                name: "write",
+                //icon: "pencil",
+                execute: () => setSelectedTab("write"),
+            },
+        ],
+    ];
+
+    return (
+        <section className="pane editor">
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+
+                //toolbarCommands={toolbarCommands}
+                toolbarCommands={[]}
+
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </section>
+    )
+}
