a78eb72354a3f3cffb95b5733a15fff8fc530608
mfyx <1571547820@qq.com>
2024-10-05 23:13:21 +0800

    初始化Broodl的主要组件

---
+++ NextJS/broodl/app/dashboard/page.js
@@ -0,0 +1,26 @@
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
+import Main from "@/components/Main";
+
+export const metadata = {
+  title: "Broodl · Dashboard",
+};
+
+export default function DashboardPage() {
+  const isAuthenticated = true; 
+
+  let children = (
+    <Login />
+    // <Dashboard />
+  )
+
+  if (isAuthenticated) {
+    children = <Dashboard />;
+  }
+
+  return (
+    <Main>
+      {children}
+    </Main>
+  );
+}
---
+++ NextJS/broodl/components/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h1>Hero</h1>
+    </div>
+  );
+}
